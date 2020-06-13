084cbe2c5231a60a28e2ea0479212510bea740d3
eugenie8 <yoojin7856@gmail.com>
2020-06-14 00:32:54 +0900

    Hangman Judge
    Online judge_489

--- 489_Hangman.cpp
+++ 489_Hangman.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
-#include <string>
 #include <set>
 
 using namespace std;
 
 int main()
 {
-    int round;
-    string x,y;
+    int n;
+    string a, b;
 
     while(1)
     {
-        cin >> round;
-        if (round == -1)
+        cin >> n;
+
+        if (n == -1)
         {
             break;
         }
-        cout << "Round " << round << endl;
-
-        cin >> x >> y;
-        set<char> voca(x.begin(), x.end());
-        set<char> answer(y.begin(), y.end());
-        
-        while(1)
+        cout << "Round " << n << endl;
+        cin >> a >> b;
+        set<char> sa(a.begin(), a.end());
+        int count(0);
+        for (auto cb : b)
         {
-            if (voca == answer)
+            if (sa.find(cb) != sa.end())
             {
-                cout << "You win." << endl;
+                sa.erase(cb);
+            }
+            else
+            {
+                count++;
+            }
+            if (count > 6 || sa.empty())
+            {
                 break;
             }
-            else if (voca != answer)
+            if (sa.empty())
+            {
+                cout << "You win." << endl;
+            }
+            else if (count > 6)
             {
                 cout << "You lose." << endl;
-                break;
-            }            
-        }
-        if (answer.size() > 6)
-        {
-            cout << "You chickend out." << endl;
+            }
+            else
+            {
+                cout << "You chickend out." << endl;
+            }
         }
     }
-
     return 0;
 }
