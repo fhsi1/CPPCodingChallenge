6c19d0e6d034c8aaf14c8fb002236b54456a4076
eugenie8 <yoojin7856@gmail.com>
2020-06-20 22:26:38 +0900

    Exercise problem
    Solving exercises

---
+++ 2/exercise/0004/0004b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    while(n > 1)
+    {
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n += 1;
+        }
+        cout << n << endl;
+        
+    }
+
+    return 0;
+}
