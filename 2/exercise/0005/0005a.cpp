4bc58fb29673c13c0f9b6670f97221cdf0184c1b
eugenie8 <yoojin7856@gmail.com>
2020-06-21 21:45:04 +0900

    Exercise problem
    Solving exercises

---
+++ 2/exercise/0005/0005a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int row, col, x;
+    cin >> row;
+    
+    vector<vector<int>> a(row);
+
+    for (auto &aa : a) // &aa?
+    {
+        cin >> col;
+        
+        while(col--)
+        {
+            cin >> x;
+            aa.push_back(x);
+        }
+    }
+
+    for (auto aa : a)
+    {
+        for (auto aaa : aa)
+        {
+            cout << aaa << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
